7e97e9f1fa84ac0f491a8abbaa633bb2982ee7bb
dzemix <czesio517619032@wp.pl>
2018-08-30 15:21:14 +0200

    upgrade axios

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/`,
+    baseURL: `http://95.49.108.169:8081/`,
     headers: {
       Authorization: `Bearer ${store.state.adminToken}`
     }
